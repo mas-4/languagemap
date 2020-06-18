69fd2e975e31813a8b90babe5af748a626faddd7
malan88 <m@stdwtr.io>
2020-06-18 14:48:23 -0400

    Get a heat map up from the website example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './App.css';
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-
-const geoUrl =
-    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+import Map from './Map';
 
 function App() {
     return (
         <div className="App">
-            <ComposableMap>
-                <Geographies geography={geoUrl}>
-                    {({ geographies }) =>
-                            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
-                    }
-                </Geographies>
-            </ComposableMap>
+            <Map />
         </div>
     );
 }
