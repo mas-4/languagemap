9886c01f26a2a7cf493019ca975980ed3f7381b1
malan88 <m@stdwtr.io>
2020-06-18 14:02:05 -0400

    First map version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+const geoUrl =
+    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ComposableMap>
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+                    }
+                </Geographies>
+            </ComposableMap>
+        </div>
+    );
 }
 
 export default App;
