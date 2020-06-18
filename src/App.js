5f7d39fd0bd555ec15221d4719e39f2409555c03
malan88 <m@stdwtr.io>
2020-06-18 13:53:47 -0400

    Remove logo from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+
+      </header>
     </div>
   );
 }
