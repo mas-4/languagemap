79462eeb4628429c532a99ea721cc6668977d0ba
malan88 <m@stdwtr.io>
2020-06-20 19:03:03 -0400

    New languages file, start rewrite with checkboxes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import Map from './Map';
 import { languages } from './languages'
 
-function App() {
-    const [data, setData] = useState([]);
-    const [currentLanguages, setCurrentLanguages] = useState([]);
-
-    function onClick(language) {
-        const tmp = data.slice()
-        const codes = languages[language].slice()
-        for (let i = 0; i < codes.length; i++) {
-            if (!tmp.includes(codes[i])) {
-                tmp.push(codes[i])
+function constructData(currentStatuses, currentLanguages) {
+    /* This function iterates throught he active statuses and the current
+     * languages to construct an array of country codes to be rendered green */
+    const codes = []
+    for (let s of currentStatuses) {
+        for (let l of currentLanguages) {
+            if (l in languages[s]) {
+                codes.push(...languages[s][l])
             }
         }
-        setData(tmp);
+    }
+    return codes
+}
+
+function App() {
+    const DFLT = ['de facto', 'majority', 'official']
+
+    const [data, setData] = useState([]);
+    const [currentLanguages, setCurrentLanguages] = useState([]);
+    const [currentStatuses, setCurrentStatuses] = useState(DFLT);
+
+    function clickLanguage(language) {
+        const tmp = currentLanguages.slice();
     }
 
     return (
@@ -23,9 +33,10 @@ function App() {
             <Map data={data}/>
             <ul>
                 {Object.keys(languages).map((key, index) => {
+                    /* statuses */
                     return(
                         <li key={key}>
-                            <button onClick={() => onClick(key)}>
+                            <button onClick={() => clickLanguage(key)}>
                                 {key}
                             </button>
                         </li>
